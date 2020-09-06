b5b6c6d9bd3aea71f6d6751f1e9e09691e230afc
幸福村的二娃 <xingfucundeerwa@xingfucundeerwadeMacBook-Pro.local>
2020-09-06 11:59:54 +0800

    新增-钉钉预警

--- quantify/run.py
+++ quantify/run.py
@@ -1,24 +1,31 @@
 from system.huobi_tool import get_market_price, buy_coin, sell_coin
 from system.price import get_buy_price, get_sell_price, modify_price
-
-import time
+from system.dingding import dingding_warn
+import time,traceback
 
 def loop_fun():
-    while True:
-        if get_buy_price() >= get_market_price("eosusdt"):
-            print("满足买价，购买价为：%f" % get_buy_price())
-            order_id=buy_coin("eosusdt",get_buy_price())
-            modify_price(get_buy_price())
-            
-        elif get_sell_price() < get_market_price("eosusdt"):
-            print("满足卖价，卖价为：%f" % get_sell_price())
-            order_id=sell_coin("eosusdt",get_sell_price())
-            modify_price(get_sell_price())
-        else:
-            print("未能交易,继续运行") 
+        while True:
+            if get_buy_price() >= get_market_price("eosusdt"):
+                print("满足买价，购买价为：%f" % get_buy_price())
+                order_id=buy_coin("eosusdt",get_buy_price())
+                modify_price(get_buy_price())
+                
+            elif get_sell_price() < get_market_price("eosusdt"):
+                print("满足卖价，卖价为：%f" % get_sell_price())
+                order_id=sell_coin("eosusdt",get_sell_price())
+                modify_price(get_sell_price())
+            else:
+                print("未能交易,继续运行") 
+          
 
 
 if __name__ == "__main__":
-    loop_fun()
+    try:
+        loop_fun()
+    except Exception as e:
+        error_info = "报警："+str(e)
+        print(error_info)
+        dingding_warn(error_info) 
     # order_id=sell_coin("eosusdt",5)
     # modify_price(get_sell_price())
+    # get_market_price("eosusdt")
---
+++ quantify/system/dingding.py
@@ -0,0 +1,31 @@
+
+import requests
+import json
+from data.author import dingding_token
+
+def msg(text):
+    json_text = {
+        "msgtype": "text",
+        "at": {
+            "atMobiles": [
+                "11111"
+            ],
+            "isAtAll": False
+        },
+        "text": {
+            "content": text
+        }
+    }
+    return json_text
+
+
+def dingding_warn(text):
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    api_url = "https://oapi.dingtalk.com/robot/send?access_token=%s" % dingding_token 
+    json_text = msg(text)
+    requests.post(api_url, json.dumps(json_text), headers=headers).content
+    
+
+
+if __name__ == '__main__':
+    pass
